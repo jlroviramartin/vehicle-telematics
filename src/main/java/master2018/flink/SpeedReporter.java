3c9a3d24d153d9f90b64a100b10f133f2666b835
Jose Luis R. M <jlroviramartin@gmail.com>
2018-11-08 18:39:13 +0100

    Se doumenta la clase, se hace final y se añaden anotaciones para aumentar la velocidad.

--- src/main/java/master2018/flink/SpeedReporter.java
+++ src/main/java/master2018/flink/SpeedReporter.java
@@ -4,9 +4,13 @@ import master2018.flink.events.PrincipalEvent;
 import master2018.flink.events.SpeedEvent;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.FunctionAnnotation;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 
-public class SpeedReporter {
+/**
+ * This class evaluates the speed fines.
+ */
+public final class SpeedReporter {
 
     /**
      * Speed limit used for calculating fines.
@@ -28,7 +32,7 @@ public class SpeedReporter {
     /**
      * This {@code MapFunction} maps from {@code PrincipalEvent} to {@code SpeedEvent}.
      */
-    //@FunctionAnnotation.ForwardedFields("0;1;3->2;6->3;5->4;2->5")
+    @FunctionAnnotation.ForwardedFields("f0; f1; f3->f2; f6->f3; f5->f4; f2->f5")
     private static final class MapToSpeedEvent implements MapFunction<PrincipalEvent, SpeedEvent> {
 
         @Override
