2f40b23e68a4703db9d0c436b9adf60656238f86
Jose Luis R. M <jlroviramartin@gmail.com>
2018-11-08 20:01:39 +0100

    Comprueba que los eventos sean consecutivos en el tiempo

--- src/main/java/master2018/flink/functions/AccidentWindowFunction.java
+++ src/main/java/master2018/flink/functions/AccidentWindowFunction.java
@@ -1,14 +1,13 @@
 package master2018.flink.functions;
 
+import java.util.Iterator;
 import master2018.flink.events.AccidentEvent;
 import master2018.flink.events.PrincipalEvent;
-import master2018.flink.libs.Utils;
 import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.shaded.com.google.common.collect.Iterables;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.util.Collector;
-
-import java.util.Iterator;
 
 public final class AccidentWindowFunction
         implements WindowFunction<PrincipalEvent, AccidentEvent, Tuple, GlobalWindow> {
@@ -16,25 +15,33 @@ public final class AccidentWindowFunction
     @Override
     public void apply(Tuple key, GlobalWindow globalWindow, Iterable<PrincipalEvent> iterable, Collector<AccidentEvent> accidents) {
 
-        if (Utils.size(iterable) == 4) {
-
+        if (Iterables.size(iterable) == 4) {
             Iterator<PrincipalEvent> events = iterable.iterator();
 
             PrincipalEvent firstEvent = events.next();
+            int time = firstEvent.getTime();
 
             PrincipalEvent lastEvent = firstEvent; // Avoid "can be null"
             while (events.hasNext()) {
                 lastEvent = events.next();
+                time += 30;
+
+                // Early exit if the events are not ordered.
+                // It could be used a boolean variable instead of an early return. This could be cleaner...
+                // as you wish my lord...
+                if (time != lastEvent.getTime()) {
+                    return;
+                }
             }
 
-            AccidentEvent accidentEvent = new AccidentEvent();
-            accidentEvent.setTime1(firstEvent.getTime());
-            accidentEvent.setTime2(lastEvent.getTime());
-            accidentEvent.setVid(firstEvent.getVid());
-            accidentEvent.setHighway(firstEvent.getHighway());
-            accidentEvent.setSegment(firstEvent.getSegment());
-            accidentEvent.setDirection(firstEvent.getDirection());
-            accidentEvent.setPosition(firstEvent.getPosition());
+            AccidentEvent accidentEvent = new AccidentEvent(
+                    firstEvent.getTime(),
+                    lastEvent.getTime(),
+                    firstEvent.getVid(),
+                    firstEvent.getHighway(),
+                    firstEvent.getSegment(),
+                    firstEvent.getDirection(),
+                    firstEvent.getPosition());
 
             accidents.collect(accidentEvent);
         }
