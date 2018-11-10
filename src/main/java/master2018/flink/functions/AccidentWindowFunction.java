984cf43a449d6e9c591416834139aade66ee7869
Jose Luis R. M <jlroviramartin@gmail.com>
2018-11-10 17:41:13 +0100

    La funcion ventana utiliza un contador en vez de Iterables.size

--- src/main/java/master2018/flink/functions/AccidentWindowFunction.java
+++ src/main/java/master2018/flink/functions/AccidentWindowFunction.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 import master2018.flink.events.AccidentEvent;
 import master2018.flink.events.PrincipalEvent;
 import org.apache.flink.api.java.tuple.Tuple;
-import org.apache.flink.shaded.com.google.common.collect.Iterables;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.util.Collector;
@@ -15,35 +14,40 @@ public final class AccidentWindowFunction
     @Override
     public void apply(Tuple key, GlobalWindow globalWindow, Iterable<PrincipalEvent> iterable, Collector<AccidentEvent> accidents) {
 
-        if (Iterables.size(iterable) == 4) {
-            Iterator<PrincipalEvent> events = iterable.iterator();
+        //if (Iterables.size(iterable) == 4) {
+        Iterator<PrincipalEvent> events = iterable.iterator();
 
-            PrincipalEvent firstEvent = events.next();
-            int time = firstEvent.getTime();
+        PrincipalEvent firstEvent = events.next();
+        int count = 1;
+        int time = firstEvent.getTime();
 
-            PrincipalEvent lastEvent = firstEvent; // Avoid "can be null"
-            while (events.hasNext()) {
-                lastEvent = events.next();
-                time += 30;
+        PrincipalEvent lastEvent = firstEvent; // Avoid "can be null"
+        while (events.hasNext()) {
+            lastEvent = events.next();
+            count++;
+            time += 30;
 
-                // Early exit if the events are not ordered.
-                // It could be used a boolean variable instead of an early return. This could be cleaner...
-                // as you wish my lord...
-                if (time != lastEvent.getTime()) {
-                    return;
-                }
+            // Early exit if the events are not ordered.
+            // It could be used a boolean variable instead of an early return. This could be cleaner...
+            // as you wish my lord...
+            if (time != lastEvent.getTime()) {
+                return;
             }
-
-            AccidentEvent accidentEvent = new AccidentEvent(
-                    firstEvent.getTime(),
-                    lastEvent.getTime(),
-                    firstEvent.getVid(),
-                    firstEvent.getHighway(),
-                    firstEvent.getSegment(),
-                    firstEvent.getDirection(),
-                    firstEvent.getPosition());
-
-            accidents.collect(accidentEvent);
         }
+
+        if (count != 4) {
+            return;
+        }
+
+        AccidentEvent accidentEvent = new AccidentEvent(
+                firstEvent.getTime(),
+                lastEvent.getTime(),
+                firstEvent.getVid(),
+                firstEvent.getHighway(),
+                firstEvent.getSegment(),
+                firstEvent.getDirection(),
+                firstEvent.getPosition());
+
+        accidents.collect(accidentEvent);
     }
 }
