afc7a54baffc4496c96f3221e9c6a5c67d3d1705
Jose Luis R. M <jlroviramartin@gmail.com>
2018-11-10 17:34:22 +0100

    Cambio en el main principal: se añaden los serializables, se indica cuando se limpia los buffers y se cambia en el retorno de AverageSpeedReporter.analyze

--- src/main/java/master2018/flink/VehicleTelematics.java
+++ src/main/java/master2018/flink/VehicleTelematics.java
@@ -2,7 +2,16 @@ package master2018.flink;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import master2018.flink.events.AccidentEvent;
+import master2018.flink.events.AccidentEventSerializer;
+import master2018.flink.events.AverageSpeedEvent;
+import master2018.flink.events.AverageSpeedEventSerializer;
+import master2018.flink.events.AverageSpeedTempEvent;
+import master2018.flink.events.AverageSpeedTempEventSerializer;
 import master2018.flink.events.PrincipalEvent;
+import master2018.flink.events.PrincipalEventSerializer;
+import master2018.flink.events.SpeedEvent;
+import master2018.flink.events.SpeedEventSerializer;
 import master2018.flink.functions.ParsePrincipalEventMapFunction;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -18,8 +27,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  * Example: master2018.flink.VehicleTelematics /srv/flink/traffic-3xways /srv/flink/out/
  * <p>
  */
-public class VehicleTelematics {
-
+public final class VehicleTelematics {
 
     public static void main(String[] args) throws Exception {
 
@@ -41,8 +49,19 @@ public class VehicleTelematics {
         // Get the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        // triggers flushing only when the output buffer is full thus maximizing throughput
+        env.setBufferTimeout(-1);
+
         // Enable object reuse to increase pereformance
         env.getConfig().enableObjectReuse();
+
+        // Kryo serializers
+        env.getConfig().enableForceKryo();
+        env.getConfig().registerTypeWithKryoSerializer(AccidentEvent.class, AccidentEventSerializer.class);
+        env.getConfig().registerTypeWithKryoSerializer(AverageSpeedEvent.class, AverageSpeedEventSerializer.class);
+        env.getConfig().registerTypeWithKryoSerializer(AverageSpeedTempEvent.class, AverageSpeedTempEventSerializer.class);
+        env.getConfig().registerTypeWithKryoSerializer(PrincipalEvent.class, PrincipalEventSerializer.class);
+        env.getConfig().registerTypeWithKryoSerializer(SpeedEvent.class, SpeedEventSerializer.class);
 
         // Ensure that the paralelism is 1 by default
         env.setParallelism(1);
@@ -54,8 +73,7 @@ public class VehicleTelematics {
 
         // Evaluates the tuples.
         SingleOutputStreamOperator<PrincipalEvent> toTuples = stream
-                .map(new ParsePrincipalEventMapFunction())
-                .setParallelism(1);
+                .map(new ParsePrincipalEventMapFunction());
 
         // Speed reporter
         SingleOutputStreamOperator speedReporter = SpeedReporter.analyze(toTuples);
@@ -64,7 +82,7 @@ public class VehicleTelematics {
                 .setParallelism(1);
 
         // Average speed reporter
-        SingleOutputStreamOperator avgspeedfines = AverageSpeedReporter.analyze(toTuples);
+        DataStream avgspeedfines = AverageSpeedReporter.analyze(toTuples);
         avgspeedfines
                 .writeAsCsv(Paths.get(outputPath, "avgspeedfines.csv").toString(), FileSystem.WriteMode.OVERWRITE)
                 .setParallelism(1);
@@ -74,7 +92,6 @@ public class VehicleTelematics {
         accidentReporter
                 .writeAsCsv(Paths.get(outputPath, "accidents.csv").toString(), FileSystem.WriteMode.OVERWRITE)
                 .setParallelism(1);
-
         env.execute("Vehicle telematics");
     }
 }
