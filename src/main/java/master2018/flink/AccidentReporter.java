cdb015d7328bcbdb038a9db7098fa89b3cc2257e
Jose Luis R. M <jlroviramartin@gmail.com>
2018-11-10 17:36:11 +0100

    Se cambia el uso de una lambda por una clase, para que sea igual que el resto. NOTA: me gusta mas los lambdas pero :( por si acaso

--- src/main/java/master2018/flink/AccidentReporter.java
+++ src/main/java/master2018/flink/AccidentReporter.java
@@ -2,6 +2,7 @@ package master2018.flink;
 
 import master2018.flink.events.PrincipalEvent;
 import master2018.flink.functions.AccidentWindowFunction;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 
 /**
@@ -12,14 +13,24 @@ public final class AccidentReporter {
     public static SingleOutputStreamOperator analyze(SingleOutputStreamOperator<PrincipalEvent> tuples) {
 
         return tuples
-                .filter(event -> event.getSpeed() == 0) // speed = 0
+                .filter(new AccidentFilterFunction()) // speed = 0
                 .keyBy(PrincipalEvent.VID,
                        PrincipalEvent.HIGHWAY,
                        PrincipalEvent.DIRECTION,
                        PrincipalEvent.SEGMENT,
                        PrincipalEvent.POSITION) // key stream by vid, highway, direction, segment, position
                 .countWindow(4, 1) // Slide window, start each one event
-                .apply(new AccidentWindowFunction())
-                .setParallelism(10);
+                .apply(new AccidentWindowFunction());
+    }
+
+    private static final class AccidentFilterFunction implements FilterFunction<PrincipalEvent> {
+
+        public AccidentFilterFunction() {
+        }
+
+        @Override
+        public boolean filter(PrincipalEvent event) throws Exception {
+            return event.getSpeed() == 0;
+        }
     }
 }
